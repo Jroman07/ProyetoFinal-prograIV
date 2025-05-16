a2f476dea30eb976bd492c78a2b7ae5ab20199ae
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-05-16 09:26:09 -0600

    cambios

---
+++ Controllers/OffersController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_PrograIV.Entities;
+using Proyecto_Final_PrograIV.Services;
+
+namespace Proyecto_Final_PrograIV.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OffersController : ControllerBase
+    {
+        private readonly IOfferService _OfferService;
+
+        public OffersController(IOfferService offerService)
+        {
+            _OfferService = offerService;
+        }
+
+        // get : api/OffersController
+        [HttpGet]
+        public IEnumerable<Offer> Get()
+        {
+            return _OfferService.GetallOffers();
+        }
+
+        // get by id
+        [HttpGet("{id}")]
+        public Offer Get(int id)
+        {
+            return _OfferService.GetOfferById(id);
+        }
+
+        [HttpGet("search")]
+        public IEnumerable<Offer> Get([FromQuery] string? name)
+        {
+            return (IEnumerable<Offer>)_OfferService.GetOfferByName(name);
+        }
+    }
+}
--- Services/OfferService.cs
+++ Services/OfferService.cs
@@ -2,11 +2,82 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Proyecto_Final_PrograIV.Entities;
+using Proyecto_Final_PrograIV.FinalProjectDataBase;
 
 namespace Proyecto_Final_PrograIV.Services
 {
-    public class OfferService
+    public class OfferService : IOfferService
     {
+
+        private readonly FinalProjectDbContext _dbContext; // atributo de la base de datos para pderla usar 
+
+        public OfferService(FinalProjectDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        } // se crea un constructor 
+
+        public Offer AddOffer(Offer offer)
+        {
+            _dbContext.Offers.Add(offer);
+            _dbContext.SaveChanges();
+
+            return offer;
+        } // añadir oferta
+
+        public void DeleteOffer(int Id)
+        {
+            Offer? DeleteOffer = _dbContext.Offers.Find(Id);
+
+            if (DeleteOffer != null){
+                _dbContext.Offers.Remove(DeleteOffer);
+
+                _dbContext.SaveChanges();
+            }else{
+                throw new Exception("Candidate not found");
+            }
+        } // borrar todos
+
+        public List<Offer> GetallOffers()
+        {
+            return _dbContext.Offers.ToList();
+        } // mostrar todos
+
+        public Offer GetOfferById(int Id)
+        {
+            Offer? offer = _dbContext.Offers.Find(Id);
+
+            if (offer != null){
+                return offer;
+            }else{
+                throw new Exception("Offer not found");
+            }
+        }
+
+        public Offer GetOfferByName(string? Name)
+        {
+            Offer? findOfferByName = _dbContext.Offers.Find(Name);
+
+            if (findOfferByName!= null){
+                return findOfferByName;
+            }else{
+                throw new Exception("Offer not found");
+            }
+
+        } // mostrar oferta por el nombre
+
+        public Offer UpdateOffer(int Id, Offer offer)
+        {
+            Offer? UpdateOffer = _dbContext.Offers.Find(Id);
+
+            if (UpdateOffer != null){
+                _dbContext.Offers.Update(UpdateOffer);
+
+                return UpdateOffer;
+            }else{
+                throw new Exception("Offer not found");
+            }
         
+        }// actualizar oferta
     }
 }
