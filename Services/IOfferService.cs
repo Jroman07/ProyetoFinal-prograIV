c3ea3deafc2dddd4a77115150c7f27710ab67bf3
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-05-16 08:53:25 -0600

    cambios importantes

--- Services/IOfferService.cs
+++ Services/IOfferService.cs
@@ -7,10 +7,21 @@ using Proyecto_Final_PrograIV.Entities;
 
 namespace Proyecto_Final_PrograIV.Services
 {
-    public class IOfferService
+    public interface IOfferService
     {
-        public List <Offer>  GetAllCandidates();
+        public List<Offer> GetallOffers();
 
+        public Offer GetOfferById(int Id);
+
+        public Offer AddOffer(Offer offer);
+
+        public Offer UpdateOffer(int Id, Offer offer);
+
+        public void DeleteOffer(int Id);
+
+        public Offer GetOfferByOffer(string? Name); 
+
+        
 
     }
 }
