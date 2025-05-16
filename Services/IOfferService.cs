cc3ed90ead63ecb45f04ad6d02ae1bdf7a71ad7b
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-05-16 08:44:51 -0600

    cambios

---
+++ Services/IOfferService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Proyecto_Final_PrograIV.Entities;
+
+
+namespace Proyecto_Final_PrograIV.Services
+{
+    public class IOfferService
+    {
+        public List <Offer>  GetAllCandidates();
+
+
+    }
+}
