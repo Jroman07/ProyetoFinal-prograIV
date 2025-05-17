df6cb6ae2506a0bed1fe6568a253de6059845cd8
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2025-05-16 23:23:26 -0600

    cambios

--- Services/ICompaniesService.cs
+++ Services/ICompaniesService.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Proyecto_Final_PrograIV.Entities;
 
 namespace Proyecto_Final_PrograIV.Services
 {
     public interface ICompaniesService
     {
-        
+        public List<Company> GetAllCompanies();
+
+
+        public Company AddCompany(Company company);
+
+        public Company UpdateCompany(int id, Company company);
+
+        public void DeleteCompany(int id);
+
+        public Company GetCompanyById(int id);
+
+         public List<Company> GetCompanyByName(string name);
     }
 }
